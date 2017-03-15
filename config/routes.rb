bdfd32989c5f907e3f86d66e29b43bc8d6a6c86d
Joshua Gelbard <gelbard.joshua@gmail.com>
2017-03-14 21:29:44 -0700

    Make :api namespace default to JSON

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :update]
     resource :session, only: [:create, :destroy]
   end
