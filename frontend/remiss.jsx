25722e852fb3a02ae6032553683da3dfd52ab985
Joshua Gelbard <gelbard.joshua@gmail.com>
2017-03-15 13:47:46 -0700

    Persist user login after refresh

--- frontend/remiss.jsx
+++ frontend/remiss.jsx
@@ -8,7 +8,13 @@ const windowFunctions = { login, logout };
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  const store = configureStore();
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+  } else {
+    store = configureStore();
+  }
   ReactDOM.render(<Root store={ store }/> , root);
   Object.assign(window, windowFunctions, { store });
 });
