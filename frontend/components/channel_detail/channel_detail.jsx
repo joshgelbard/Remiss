1558e5d8cfa0c65336959758674e718640096824
Joshua Gelbard <gelbard.joshua@gmail.com>
2017-03-21 10:39:21 -0700

    Add placeholder ChannelDetailDisplay

--- frontend/components/channel_detail/channel_detail.jsx
+++ frontend/components/channel_detail/channel_detail.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
 
+const ChannelDetailHeader = ({ channel }) => {
+  return (
+    <h1 className='channel-detail-header'>About #{channel.name}</h1>
+  );
+};
+
+const ChannelInfo = ({ channel }) => {
+  return (
+    <div className='channel-info'>
+      <h2>Channel Details</h2>
+      <h3>Purpose</h3>
+      <p>{channel.purpose}</p>
+      <p>Created by someone on date</p>
+    </div>
+  );
+};
+
+const MembersList = ({ channel }) => {
+  const members = channel.members.map( (m, idx) => (
+    <li key={`member-${idx}`}>
+      {m.username}
+    </li>
+  ));
+  return (
+    <ul>
+      { members }
+    </ul>
+  );
+};
+
 class ChannelDetail extends React.Component {
   render(){
+    const { channel } = this.props;
     return (
       <div className='channel-detail'>
-        <p>Channel details and such</p>
+        <ChannelDetailHeader channel={channel} />
+        <ChannelInfo channel={channel} />
+        <MembersList channel={channel} />
       </div>
     );
   }
